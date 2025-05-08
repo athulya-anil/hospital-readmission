74e1c576e7a67ecb18c60a51a65acd72f64212e3
athulya anil <athulyaanil@Athulyas-MacBook-Pro.local>
2025-05-08 00:38:13 -0400

    Base url change

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/hospital-readmission">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>,
